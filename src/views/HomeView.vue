94c047a88ec8bbc7b935a66aecaa549f2caba479
jenkinchris11 <jenkinchris11@gmail.com>
2023-02-09 20:00:18 +0000

    Meta Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <meta property="og:image" content="https://scontent-lcy1-1.xx.fbcdn.net/v/t39.30808-6/325792514_513629890869331_8323829041432717049_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=qVaOROpVZo4AX8GEt4A&_nc_ht=scontent-lcy1-1.xx&oh=00_AfDzCO1HdakOY0FrPTheTBNSqrSwktigX9YhsxXbZDODiA&oe=63E0DBD0">
   <meta name="googlebot" content="noindex">
   <meta name="robots" content="noindex">
+  <meta name="description" content="Professional wedding and portrait photographer traveling the uk in my little camper shooting weddings">
   <link href="styles/ihover.css" rel="stylesheet">
   <div class="no-scroll">
     <div class="skw-pages" style="font-family: 'Gruppo'">
