6cc9d7e0855dc84ebf7ed8db7bf4e52acda2af22
jenkinchris11 <jenkinchris11@gmail.com>
2023-02-26 13:12:03 +0000

    Meta

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template class="no-scroll" style="background: #f4ece0;">
   <meta property="og:image" content="https://scontent-lcy1-1.xx.fbcdn.net/v/t39.30808-6/325792514_513629890869331_8323829041432717049_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=qVaOROpVZo4AX8GEt4A&_nc_ht=scontent-lcy1-1.xx&oh=00_AfDzCO1HdakOY0FrPTheTBNSqrSwktigX9YhsxXbZDODiA&oe=63E0DBD0" />
   <meta name="description" content="Professional wedding and portrait photographer traveling the uk in my little camper shooting weddings">
+  <meta name="google-site-verification" content="H7n1yxjk1UMuusSC0VE8oYdYA8G8wM0RXNvtuPPfutk" />
   <div style="background: #f4ece0; width: 100vw; height: 100vh; overflow-x: hidden">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
