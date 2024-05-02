fd5230e03205c088772f318883e6d8b5fa199859
jenkinchris11 <jenkinchris11@gmail.com>
2024-05-02 14:34:24 +0100

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -31,10 +31,12 @@
             <br>
             I am confident in my role from being in the industry for years. And I like to think of myself as friendly, approachable, professional and always happy to help.
             <br>
-            <img style="height: 25vh" :src="require('@/assets/finalist.png')">
-            <br>
-            <img style="height: 25vh" :src="require('@/assets/industry.jpg')">
-          </p>
+              <div>
+                <img style="height: 25vh" :src="require('@/assets/finalist.png')">
+                <br>
+                <img style="height: 25vh" :src="require('@/assets/industry.jpg')">
+              </div>  
+            </p>
       </div>
         <div>
 
