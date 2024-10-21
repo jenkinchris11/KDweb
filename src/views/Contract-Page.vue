b75188d0c2304b30aa8732f84868bb5163e4df4e
jenkinchris11 <jenkinchris11@gmail.com>
2024-10-21 14:15:30 +0100

    API Key Security

--- src/views/Contract-Page.vue
+++ src/views/Contract-Page.vue
@@ -543,7 +543,7 @@ export default {
       correctCode2: 'CODE456',
       serviceID: 'service_hk3ve7b', // Replace with your EmailJS service ID
       apiKey: 'ed5t0jwfcZmEcYl_4', // Replace with your EmailJS API key
-      dropboxAccessToken: 'sl.B_P_qsgGhAcm5qvn2tQPZb_sOqDEdn2oY4YCylG0brahL5nnr7YwlVMzUD51AapwhJsvTFy6pHD02bk_A2tHptNm94ucOcdCMKVRvvQojPwt5CJZzSj7aAbpxCCnHxSx-_SNwfzuaQIOgN0', // Replace with your Dropbox Access Token
+      dropboxAccessToken: process.env.API_KEY, // Replace with your Dropbox Access Token
       signaturePadData: null,
     };
   },
